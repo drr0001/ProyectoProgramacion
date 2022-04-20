03091c46ac480ef5e79dba3b2ce9afd52d30b094
Alumno <Alumno@DESKTOP-8RM3RIB>
2022-04-20 18:52:14 +0200

    PARTE

--- src/model/Revision.java
+++ src/model/Revision.java
@@ -10,11 +10,26 @@ package model;
  * @author Grupo Ejer4
  */
 public class Revision {
+
     private int codigo;
     private String fecha;
     private boolean filtro;
     private boolean aceite;
     private boolean frenos;
 
-    
+    public Revision() {
+        this.codigo = 0;
+        this.fecha = null;
+        this.filtro = false;
+        this.aceite = false;
+        this.frenos = false;
+            
+    }
+    public Revision(int codigo, String fecha, boolean filtro, boolean aceite, boolean frenos) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.filtro = filtro;
+        this.aceite = aceite;
+        this.frenos = frenos;
+    }
 }
